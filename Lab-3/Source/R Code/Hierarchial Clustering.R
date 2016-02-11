daa10c654f1d79b0b80604eb77fc5a7d760bdd59
vamsikrishnabhuvanam <vbp9d@mail.umkc.edu>
2016-02-11 16:53:20 -0600

    Lab -3
    Tutorial

--- Lab-3/Source/R Code/Hierarchial Clustering.R
+++ Lab-3/Source/R Code/Hierarchial Clustering.R
@@ -2,100 +2,53 @@ food <- read.table("http://www.stat.sc.edu/~hitchcock/foodstuffs.txt", header=T)
 
 attach(food)
 
-# The hclust function requires that a distance object be input:
-
-# Let's first scale the data by dividing each variable by its standard deviation:
-
-std <- apply(food[,-1], 2, sd) # finding standard deviations of variables
+std <- apply(food[,-1], 2, sd)
 food.std <- sweep(food[,-1],2,std,FUN="/") 
-
-# Calculating pairwise Euclidean distances between the (standardized) objects:
 
 dist.food <- dist(food.std)
 
-# Single linkage:
-
 food.single.link <- hclust(dist.food, method='single')
-
-# Plotting the single linkage dendrogram:
 
 plclust(food.single.link, labels=Food, ylab="Distance")
 
-windows() # opening new window while keeping previous one open
-
-# complete linkage:
-
+windows()
 food.complete.link <- hclust(dist.food, method='complete')
-
-# Plotting the complete linkage dendrogram:
 
 plclust(food.complete.link, labels=Food, ylab="Distance")
 
-windows() # opening new window while keeping previous one open
-
-# Average linkage:
+windows()
 
 food.avg.link <- hclust(dist.food, method='average')
 
-# Plotting the average linkage dendrogram:
-
 plclust(food.avg.link, labels=Food, ylab="Distance")
 
-# Note the complete linkage algorithm is slightly less prone to forming 
-# "outlier-only" clusters here.
-
-# Cutting the complete-linkage dendrogram to form k=2 clusters here:
-
 cut.2 <- cutree(food.complete.link, k=2)
-cut.2     # printing the "clustering vector"
+cut.2 
 
 food.2.clust <- lapply(1:2, function(nc) Food[cut.2==nc])  
-food.2.clust   # printing the clusters in terms of the Food labels
-
-# Suppose we preferred a 5-cluster solution:
+food.2.clust
 
 cut.5 <- cutree(food.complete.link, k=5)
 
-# Equivalently, in this case:
 cut.5 <- cutree(food.complete.link, h=3.5)  
-# h specifies the height at which the dendrogram should be cut
 
-cut.5   # printing the "clustering vector"
+cut.5
 
 food.5.clust <- lapply(1:5, function(nc) Food[cut.5==nc])  
-food.5.clust   # printing the clusters in terms of the Food labels
-
-
-############# Visualization of Clusters:
-
-### Via the scatterplot matrix:
+food.5.clust
 
 pairs(food[,-1], panel=function(x,y) text(x,y,cut.5))
 
-# Cluster 1 seems to be the high-fat, high-energy foods (beef, ham, pork)
-# Cluster 2 foods seem to have low iron (more white meats than red meats)
-# Cluster 4 foods have low protein (the clams)
-# Cluster 5 is a high-calcium outlier (canned sardines)
-
-### Via a plot of the scores on the first 2 principal components, 
-### with the clusters separated by color:
-
 food.pc <- princomp(food[,-1],cor=T)
 
-# Setting up the colors for the 5 clusters on the plot:
 my.color.vector <- rep("green", times=nrow(food))
 my.color.vector[cut.5==2] <- "blue"
 my.color.vector[cut.5==3] <- "red"
 my.color.vector[cut.5==4] <- "orange"
 my.color.vector[cut.5==5] <- "brown"
 
-# Plotting the PC scores:
-
 par(pty="s")
 plot(food.pc$scores[,1], food.pc$scores[,2], ylim=range(food.pc$scores[,1]), 
      xlab="PC 1", ylab="PC 2", type ='n', lwd=2)
 text(food.pc$scores[,1], food.pc$scores[,2], labels=Food, cex=0.7, lwd=2,
      col=my.color.vector)
-
-# What would this plot look like for the 2-cluster solution?
-# For the 3-cluster solution?
--- Lab-3/Source/R Code/K-Medoids.R
+++ Lab-3/Source/R Code/K-Medoids.R
@@ -1,37 +1,11 @@
-##########################
-##
-##  K-medoids clustering
-##
-##########################
-
-
-###########################################
-###  Cars example 
-###########################################
-
-
-# Consider the cars.data and cars.std data frames we created above.
-
-# Let's cluster the cars into k groups using the K-medoids approach.
-
-# The function "pam" is in the "cluster" package.
-
-# Loading the "cluster" package:
-
 library(cluster)
 
-# K-medoids directly on the (standardized) data matrix:
 cars.kmed.3 <- pam(cars.std, k=3, diss=F)
 
-# Or you can do K-medoids by inputting the distance matrix:
-# cars.kmed.3 <- pam(dist.cars, k=3, diss=T)
+cars.kmed.3$clustering
 
-cars.kmed.3$clustering  # printing the "clustering vector"
+cars.kmed.3$silinfo$avg.width
 
-cars.kmed.3$silinfo$avg.width  #printing the average silhouette width
-
-### A little function to calculate the average silhouette width
-### for a variety of choices of k:
 
 my.k.choices <- 2:8
 avg.sil.width <- rep(0, times=length(my.k.choices))
@@ -40,50 +14,14 @@ avg.sil.width[ii] <- pam(cars.std, k=my.k.choices[ii])$silinfo$avg.width
 }
 print( cbind(my.k.choices, avg.sil.width) )
 
-# A LARGE average silhouette width indicates that the observations are properly clustered.
-
-# Maybe k=2 is the best choice of k here?
-
-
 cars.3.clust <- lapply(1:3, function(nc) row.names(cars.data)[cars.kmed.3$clustering==nc])  
-cars.3.clust   # printing the clusters in terms of the car names
-
-
-# Cluster 1 seems to be mostly compact cars, Cluster 2 is sports cars, Cluster 3 is large luxury sedans
-
-############# Visualization of Clusters:
-
-## Built-in plots available with the pam function:
-
-# The "clusplot":
+cars.3.clust
 
 plot(cars.kmed.3, which.plots=1)
 
-# The clusplot (in the "cluster" library) can actually be used with 
-# any clustering partition by entering the data set and the clustering vector, e.g.:
-
 clusplot(food[,-1], food.k4$cluster)
 
-# The "silhouette plot":
-
 plot(cars.kmed.3, which.plots=2)
-
-# This shows which observations are "best clustered."
-
-
-####################################################
-####################################################
-# 
-# Choosing the number of clusters k using the average silhouette width criterion.
-#
-####################################################
-####################################################
-
-# When using pam, the output will give you the average silhouette width (see above code).
-
-# We can also get the average silhouette width when using other algorithms:
-
-### With a hierarchical method (Complete linkage here):
 
 dist.food <- dist(food.std)
 food.complete.link <- hclust(dist.food, method='complete')
@@ -100,23 +38,6 @@ summary(silhouette(kmeans(food.std, centers=3, iter.max=100, nstart=25)$cluster,
 summary(silhouette(kmeans(food.std, centers=4, iter.max=100, nstart=25)$cluster, dist(food.std)))$avg.width
 summary(silhouette(kmeans(food.std, centers=5, iter.max=100, nstart=25)$cluster, dist(food.std)))$avg.width
 
-# In each case, we might choose the value of k associated with the LARGEST average silhouette width.
-
-################################
-################################
-## 
-##  Plotting the WSS for several choices of k
-##
-################################
-################################
-
-# This is a recommended method for choosing k in K-means clustering.
-
-# For the cars data, let's consider letting k vary up to 5.
-
-### CODE FOR WSS PLOT BEGINS HERE ###
-##
-#Enter name of the data matrix to be clustered here:
 my.data.matrix <- cars.std  
 
 my.k.choices <- 2:5
@@ -129,7 +50,3 @@ for(i in my.k.choices) {
 }
 wss <- c(wss1,wss)
 plot(c(1,my.k.choices),wss,type='l',xlab='Number of clusters', ylab='Within-groups sum-of-squares', lwd=2)
-##
-### CODE FOR WSS PLOT ENDS HERE ###
-
-# For what value of k does the elbow of the plot occur?
